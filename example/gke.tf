6cc5c6bff1b977a2292ba90ca9dcb346a2a3f835
Abdalluh Mostafa <abdalluh.mostafa@gmail.com>
2023-10-07 18:53:51 +0300

    names

--- example/gke.tf
+++ example/gke.tf
@@ -1,9 +1,9 @@
 module "gke_cluster" {
   source      = "../gke-module/"
-  project  = "nana-sre"
+  project  = "your_project_id"
   region      = "us-central1"
-  cluster_name = "mygke"
-  node_pool_name = "my-nodepool"
-  node_pool_size = 2
+  cluster_name = "your_cluster_name"
+  node_pool_name = "your_node_pool_name"
+  node_pool_size = 3
   machine_type = "n1-standard-2"
 }
